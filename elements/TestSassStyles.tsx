b923110e8c1cf73b64638e54484816ac8b9f28ef
Yura <linetsky.yura@gmail.com>
2022-11-07 16:11:13 +0200

    tested style

--- elements/TestSassStyles.tsx
+++ elements/TestSassStyles.tsx
@@ -2,6 +2,8 @@ import styles from './TestSassStyles.module.sass'
 
 export const TestSassStyles = () => {
     return (
-        <div className={styles.test}>hello green text</div>
+        <div className={styles.container}>
+            <div className={styles.logo}>Rozetka</div>
+        </div>
     )
 }
